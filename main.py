3cc40be7e38eef3d8729cef1899b3c9ef9027801
Saad Chaouki <sd.chaouki@gmail.com>
2021-06-20 11:55:15 +0100

    Completed training process

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import torchvision.transforms as transforms
+import torch
+import torch.nn as nn
 import numpy as np
 
 from utils.processing import DataManager, QuickDraw, Rescale, Normalize
+from utils.models import Discriminator, Generator, GAN
 from utils.helpers import display_image
 
 if __name__ == '__main__':
     # Set the name of the dataset
-    drawing_name = 'zigzag.npy'
+    drawing_name = 'star.npy'
 
     # Parameters
-    data_limit = 200
+    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    data_limit = 50000
     image_size = 28
 
     # Download the data if it does not exist.
@@ -17,11 +21,28 @@ if __name__ == '__main__':
     data_manager.download(drawing_name)
     raw_data = data_manager.load(drawing_name, data_limit=data_limit)
 
-    # Reshaping data to 2D array of size 28x28
-    reshaped_data = raw_data.reshape((len(raw_data), image_size, image_size))
+    # Reshaping data to 3D array of size 1x28x28
+    reshaped_data = raw_data.reshape((len(raw_data), 1, image_size, image_size))
     del raw_data
 
     # Creating the Quick, Draw! torch dataset
     quick_draw = QuickDraw(reshaped_data, transform=transforms.Compose([Rescale(255), Normalize()]))
     del reshaped_data
+
+    # Data Loader
+    data_loader = torch.utils.data.DataLoader(quick_draw, batch_size=64, shuffle=True)
+
+    # Models - Discriminator and Generator
+    discriminator = Discriminator()
+    generator = Generator()
+
+    # Create GAN model
+    gan = GAN(generator=generator, discriminator=discriminator, device=device)
+
+    # Train model
+    gan.train(data_loader, num_epochs=10)
+
+    # Save the trained model
+    gan.save(f'models/star/')
+
 
