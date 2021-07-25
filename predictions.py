409af595e327f800bdbc293ac20260649ead40e5
Saad Chaouki <sd.chaouki@gmail.com>
2021-07-25 10:31:23 +0100

    Predictions code

--- predictions.py
+++ predictions.py
@@ -4,16 +4,18 @@ from utils.helpers import display_image
 
 
 if __name__ == '__main__':
+    # Data
+    dataset = 'star'
 
     # Load the trained model
     generator = Generator()
-    generator.load_state_dict(torch.load('models/star_generator.pt'))
+    generator.load_state_dict(torch.load(f'models/{dataset}/generator.pt'))
     generator.eval()
 
     # Generate image
     image = generator.generate().detach().numpy()
 
     # Draw
-    display_image(image[1][0], reshape=False)
+    display_image(image[0][0], reshape=False)
 
 
