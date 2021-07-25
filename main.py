0f63af9111c38b6ea74739ee1aa296744d0af793
Saad Chaouki <sd.chaouki@gmail.com>
2021-07-25 10:31:42 +0100

    Adjusted code

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import torchvision.transforms as transforms
 import torch
-import torch.nn as nn
-import numpy as np
 
 from utils.processing import DataManager, QuickDraw, Rescale, Normalize
 from utils.models import Discriminator, Generator, GAN
-from utils.helpers import display_image
 
 if __name__ == '__main__':
-    # Set the name of the dataset
-    drawing_name = 'star.npy'
-
     # Parameters
-    device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
-    data_limit = 50000
-    image_size = 28
+    parameters = {
+        'dataset': 'star',
+        'device': torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu"),
+        'data_limit': 10000,
+        'image_size': 28,
+        'batch_size': 64,
+        'num_epochs': 5,
+        'learning_rate': .001
+    }
+
+    # Set the name of the dataset
+    drawing_name = f'{parameters["dataset"]}.npy'
 
     # Download the data if it does not exist.
     data_manager = DataManager()
     data_manager.download(drawing_name)
-    raw_data = data_manager.load(drawing_name, data_limit=data_limit)
+    raw_data = data_manager.load(drawing_name, data_limit=parameters['data_limit'])
 
     # Reshaping data to 3D array of size 1x28x28
-    reshaped_data = raw_data.reshape((len(raw_data), 1, image_size, image_size))
+    reshaped_data = raw_data.reshape((len(raw_data), 1, parameters['image_size'], parameters['image_size']))
     del raw_data
 
     # Creating the Quick, Draw! torch dataset
@@ -30,19 +33,23 @@ if __name__ == '__main__':
     del reshaped_data
 
     # Data Loader
-    data_loader = torch.utils.data.DataLoader(quick_draw, batch_size=64, shuffle=True)
+    data_loader = torch.utils.data.DataLoader(quick_draw, batch_size=parameters['batch_size'], shuffle=True)
 
     # Models - Discriminator and Generator
     discriminator = Discriminator()
     generator = Generator()
 
+    # Set the weights
+
+
     # Create GAN model
-    gan = GAN(generator=generator, discriminator=discriminator, device=device)
+    gan = GAN(generator=generator, discriminator=discriminator,
+              device=parameters['device'], learning_rate=parameters['learning_rate'])
 
     # Train model
-    gan.train(data_loader, num_epochs=10)
+    gan.train(data_loader, num_epochs=parameters['num_epochs'])
 
     # Save the trained model
-    gan.save(f'models/star/')
+    gan.save(f'models/{parameters["dataset"]}/')
 
 
